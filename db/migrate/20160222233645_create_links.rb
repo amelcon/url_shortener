f030af209c8a4ea52484de55871eb9476ee18658
Alex Melcon <amelc001@fiu.edu>
2016-02-22 19:01:59 -0500

    init

---
+++ db/migrate/20160222233645_create_links.rb
@@ -0,0 +1,21 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :in_url
+      t.text :out_url
+      t.integer :http_status
+
+      t.timestamps null: false
+    end
+  end
+  def up
+    create_table :links do |t|
+      t.string :in_url
+      t.text :out_url
+      t.integer :http_status, :default => 301 # default to :permanent
+      t.timestamps
+    end
+
+    add_index :links, :in_url
+  end
+end
